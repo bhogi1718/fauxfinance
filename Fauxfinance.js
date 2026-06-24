fd9ea6e1f79b56faf5c8cd861216776d27d4aceb
Charan Tej <bhogicharantej@gmail.com>
2026-06-24 16:11:30 +0530

    Initial commit

---
+++ Fauxfinance.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("searchbar").size = "10";
+}
+function stock(){
+    window.location.href = "holdings.html";
+}
+function explore(){
+    window.location.href = "holdings.html";
+}
---
+++ faqs.js
@@ -0,0 +1,115 @@
+function explore(){
+    window.location.href = "holdings.html";
+}
+
+function invest(){
+    window.location.href = "investment.html";
+}
+function faq1() {
+    let x1 = document.getElementById("A1").innerHTML;
+    document.getElementById("1").innerHTML =x1;
+    
+}
+
+function faq11(){
+    let y1 = document.getElementById("1").innerHTML;
+    document.getElementById("A1").innerHTML =y1;
+}
+
+function faq2() {
+    let x2 = document.getElementById("A2").innerHTML;
+    document.getElementById("2").innerHTML =x2;
+    
+}
+
+function faq22(){
+    let y2 = document.getElementById("2").innerHTML;
+    document.getElementById("A2").innerHTML =y2;
+}
+function faq3() {
+    let x3 = document.getElementById("A3").innerHTML;
+    document.getElementById("3").innerHTML =x3;
+    
+}
+
+function faq33(){
+    let y3 = document.getElementById("3").innerHTML;
+    document.getElementById("A3").innerHTML =y3;
+}
+
+function faq4() {
+    let x4 = document.getElementById("A4").innerHTML;
+    document.getElementById("4").innerHTML =x4;
+    
+}
+
+function faq44(){
+    let y4 = document.getElementById("4").innerHTML;
+    document.getElementById("A4").innerHTML =y4;
+}
+
+function showmore(){
+   let z=document.getElementById("sm").innerHTML;
+   document.getElementById("sm1").innerHTML =z;
+}
+
+function faq5() {
+    let x5 = document.getElementById("A5").innerHTML;
+    document.getElementById("5").innerHTML =x5;
+    
+}
+
+function faq55(){
+    let y5 = document.getElementById("5").innerHTML;
+    document.getElementById("A5").innerHTML =y5;
+}
+
+function faq6() {
+    let x6 = document.getElementById("A6").innerHTML;
+    document.getElementById("6").innerHTML =x6;
+}
+
+function faq66(){
+    let y6 = document.getElementById("6").innerHTML;
+    document.getElementById("A6").innerHTML =y4;
+}
+
+function faq7() {
+    let x7 = document.getElementById("A7").innerHTML;
+    document.getElementById("7").innerHTML =x7;
+}
+
+function faq77(){
+    let y7 = document.getElementById("7").innerHTML;
+    document.getElementById("A7").innerHTML =y7;
+}
+
+function faq8() {
+    let x8 = document.getElementById("A8").innerHTML;
+    document.getElementById("8").innerHTML =x8;
+}
+
+function faq88(){
+    let y8 = document.getElementById("8").innerHTML;
+    document.getElementById("A8").innerHTML =y8;
+}
+
+function faq9() {
+    let x9 = document.getElementById("A9").innerHTML;
+    document.getElementById("9").innerHTML =x9;
+}
+
+function faq99(){
+    let y9 = document.getElementById("9").innerHTML;
+    document.getElementById("A9").innerHTML =y9;
+}
+
+function faq0() {
+    let x0 = document.getElementById("A0").innerHTML;
+    document.getElementById("0").innerHTML =x0;
+}
+
+function faq00(){
+    let y0 = document.getElementById("0").innerHTML;
+    document.getElementById("A0").innerHTML =y0;
+}
